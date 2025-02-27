b14ae4384efff21da06ead25b91290943283bbf3
ZHENG Zian <22231153@life.hkbu.edu.hk>
2025-02-27 14:27:52 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/zianzheng.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.zianzheng
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Andy ZHENG.");
+        println("I am a student at HKBU.");
+        println("I am studying Computer Science.");
+        println("I am interested in Artificial Intelligence.");
+    }
+}
