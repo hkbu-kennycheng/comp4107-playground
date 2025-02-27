612fe397776624b1a92e6ff454b717b0a8a58c7a
hernylin1120 <lchhenry2004@gmail.com>
2025-02-27 13:54:47 +0800

    Add self-introduction feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package hk.edu.hkbu.comp.comp4107.playground
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+    val name = "Lin Ching Ho"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
