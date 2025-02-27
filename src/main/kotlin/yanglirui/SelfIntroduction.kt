34f2abe2519d677a6bc44ed84bb7bbc2b8c21dad
Yang Lirui <22257853@life..hkbu.edu.hk>
2025-02-27 14:04:31 +0800

    Resolve merge conflicts

--- src/main/kotlin/yanglirui/SelfIntroduction.kt
+++ src/main/kotlin/yanglirui/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.yanglirui
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is Kenny Cheng.")
+        println("Hello, my name is Yang LiRui.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
