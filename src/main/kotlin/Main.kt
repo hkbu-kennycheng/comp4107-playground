71703ea4ed8c9aa7d53015041c6ba214edd91924
jackyng <kkll22kkll22kkll22@gmail.com>
2025-02-27 13:37:42 +0800

    Add self-introduction feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,22 @@ package hk.edu.hkbu.comp.comp4107.playground
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+class SelfIntroduction {
+    // Define the selfIntroduction method
+    fun selfIntroduction(name: String) {
+        println("Hello, $name!")
+    }
+}
 fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val introduction = SelfIntroduction()
+
+    // Call the selfIntroduction method
+    introduction.selfIntroduction(name)
+
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
