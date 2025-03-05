9a3d27840aad4ae9ce0e64854364596b8c2ac1c7
LoTszChun <lostnightz38@gmail.com>
2025-03-06 07:28:12 +0800

    新增自我介紹功能

---
+++ src/main/kotlin/lotszchun.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.lotszchun
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Lo Tsz Chun.")
+        println("I am a student of Hong Kong Baptist University.")
+        println("I am studying computer science.")
+        println("I am interested in mobile app development and backend engineering.")
+    }
+}
