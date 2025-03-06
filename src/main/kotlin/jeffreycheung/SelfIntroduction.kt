049d13d885cf38c4617156f3a9c356d0b2779198
jeffreycheung <jeffrey2004cheung@hotmail.com>
2025-03-06 15:08:14 +0800

    commit2

--- src/main/kotlin/jeffreycheung/SelfIntroduction.kt
+++ src/main/kotlin/jeffreycheung/SelfIntroduction.kt
@@ -2,6 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.jeffreycheung
 
 class SelfIntroduction {
     fun selfIntroduction() {
+
         println("Hello, my name is Jeffrey Cheung.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
