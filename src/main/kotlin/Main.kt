db060330de9876b06a659d478304f06d18f9535d
Myselfssbrret <noelraight698@gmail.com>
2025-03-06 08:09:18 +0800

    Update Main.kt to call selfIntroduction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package hk.edu.hkbu.comp.comp4107.playground
+import hk.edu.hkbu.comp.comp4107.playground.Myselfssbrret.SelfIntroduction
 
 import hk.edu.hkbu.comp.comp4107.playground.kennycheng.SelfIntro
 
-import hk.edu.hkbu.comp.comp4107.playground.lotszchun.SelfIntroduction
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val name = "Kotlin"
     val intro = SelfIntroduction()
     intro.selfIntroduction()
+
 }
