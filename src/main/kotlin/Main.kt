aa852521cdcbda75d386aeddbdf58975da1785aa
TSE Yuk Ling <lindatse1024@gmail.com>
2025-03-06 14:34:27 +0800

    Done Lab6 Ex.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package hk.edu.hkbu.comp.comp4107.playground
 
-import hk.edu.hkbu.comp.comp4107.playground.lindatse.SelfIntroduction
+import lindatse.SelfIntroduction
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
